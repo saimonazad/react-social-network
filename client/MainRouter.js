84c6ba0dcffb13596177b8f13d33b2d6366ebf2b
saimon <saimon.azad@hotmail.com>
2020-09-20 20:54:06 +0600

    Profile view added

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -4,6 +4,9 @@ import Home from './core/Home'
 import Users from './user/Users'
 import Signup from './user/Signup'
 import Signin from './auth/Signin'
+import Profile from './user/Profile'
+import EditProfile from './user/EditProfile'
+
 
 const MainRouter = () => {
     return (
@@ -13,6 +16,8 @@ const MainRouter = () => {
                 <Route path="/users" component={Users} />
                 <Route path="/signup" component={Signup} />
                 <Route path="/signin" component={Signin} />
+                <Route path="/user/:userId" component={Profile} />
+                <PrivateRoute path="/user/edit/:userId" component={EditProfile} />
             </Switch>
         </div>
     )
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -63,6 +63,7 @@ UserSchema.methods = {
     },
     makeSalt: function () {
         return Math.round((new Date().valueOf() * Math.random())) + ''
+        
     }
 }
 
